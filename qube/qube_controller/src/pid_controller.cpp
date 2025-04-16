8d15962305ee75093512d5a06b1544cb1f376510
annaina <143585907+annaina@users.noreply.github.com>
2025-04-16 17:52:38 +0200

    Update pid_controller.cpp

--- qube/qube_controller/src/pid_controller.cpp
+++ qube/qube_controller/src/pid_controller.cpp
@@ -1,9 +1,12 @@
 #include "qube_controller/pid_controller.hpp"
 
+// Constructor for the PIDController class
 PIDController::PIDController(double p, double i, double d, double reference):
     p_(p), i_(i), d_(d), reference_(reference),
     output_(0.0), integral_(0.0), previous_error_(0.0) {}
 
+// Takes the current measured position and velocity
+// Returns the control output (velocity command)
 double PIDController::update(double measured_value, double measured_velocity) {
     const double dt = 0.01;
     double error = reference_ - measured_value;
