599ac72f4b0cf24621264564de059a3d58db0e1f
annaina <143585907+annaina@users.noreply.github.com>
2025-04-17 20:43:55 +0200

    Update pid_controller_node.hpp

--- qube/qube_controller/include/qube_controller/pid_controller_node.hpp
+++ qube/qube_controller/include/qube_controller/pid_controller_node.hpp
@@ -1,20 +1,27 @@
 #ifndef PID_CONTROLLER_NODE_HPP
 #define PID_CONTROLLER_NODE_HPP
 
-#include "pid_controller.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/joint_state.hpp"
-#include "std_msgs/msg/float64_multi_array.hpp"
+#include "pid_controller.hpp"    
+#include "rclcpp/rclcpp.hpp" // ROS2 C++ client library
+#include "sensor_msgs/msg/joint_state.hpp" 
+#include "std_msgs/msg/float64_multi_array.hpp" 
 
 class PIDControllerNode : public rclcpp::Node {
 public:
+    // Constructor: Initializes the Node, declares parameters, and sets up subscriptions and publications
     PIDControllerNode();
 
 private:
+    // Callback function for handling incoming joint state messages
     void joint_states_listener(const sensor_msgs::msg::JointState::SharedPtr msg);
-    
+
+    // PID controller instance for computing the velocity command
     std::unique_ptr<PIDController> pid_controller_;
+
+    // Publisher for sending computed velocity commands
     rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr velocity_publisher_;
+
+    // Subscriber for receiving joint states (e.g., position and velocity) from a robot or simulation
     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_states_subscriber_;
 };
 
